13b0ead7bf4ea4f1fc6411ed6cdfb085d73bd355
Rob Wood <rgwoodiii@gmail.com>
2022-02-12 22:02:15 -0700

    updates

--- starter/results/outputs/post_test_two.py
+++ starter/results/outputs/post_test_two.py
@@ -9,21 +9,21 @@ URL = "http://127.0.0.1:8000/predict/"
 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
 payload = {
-        "age": 37,
+        "age": 38,
         "workclass": "Private",
-        "fnlgt": 202683,
-        "education": "Some-college",
-        "education-num": 10,
-        "marital-status": "Married-civ-spouse",
-        "occupation": "Sales",
+        "fnlgt": 374524,
+        "education": " Bachelors",
+        "education-num": 13,
+        "marital-status": " Married-civ-spouse",
+        "occupation": " Prof-specialty",
         "relationship": "Husband",
         "race": "White",
         "sex": "Male",
         "capital-gain": 0,
         "capital-loss": 0,
-        "hours-per-week": 48,
-        "native-country": "United-States"
-    }
+        "hours-per-week": 40,
+        "native-country": " United-States"
+}
 
 response = requests.request("POST", URL, headers=headers, dta = payload)
 print(response.text)
